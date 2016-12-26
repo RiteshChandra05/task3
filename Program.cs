bdb282f5790a20cd6ddb975a076cf2f576171420
RiteshChandra05 <kesha.riteshchandragupta@gmail.com>
2016-12-26 14:42:06 -0600

    array list

--- Program.cs
+++ Program.cs
@@ -3,38 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
-namespace proctd
+namespace arrylist
 {
-     class customer
+    class arylist
     {
-        private int num = 10;
-         public void ShowMessage()
-        {
-            Console.WriteLine("Private Variable num not accessible");
-        }
-        public string s = "Hello";
-        protected int Id=1001230;
-    }
-     class cccustomer : customer
-    {
-       public void show()
-         {
-             Console.WriteLine("Protected variable :" + Id);
-         }
-    }
-    class Program
-    {
-           
         static void Main(string[] args)
         {
-            customer c2 = new customer();
-            cccustomer c3 = new cccustomer();
-            //Console.WriteLine(c2.num); num is not accessible since it is private
-            Console.WriteLine("Public variable: " + c2.s);
-            c3.show();
-            c2.ShowMessage();
-            
+            ArrayList al = new ArrayList();
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            Console.WriteLine(al.Capacity);
+            Console.WriteLine(al.Count);
         }
     }
 }
